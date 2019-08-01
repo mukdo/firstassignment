8e0b50eca467ab0ccdb1e9364c5259963d7ff417
mukdo <bmukdo@gmail.com>
2019-08-01 23:00:51 +0600

    first commit

---
+++ dev skill/first assigment/firstAssignment/firstAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstAssignment
+{
+    class Program
+    {
+       public int adder()
+        {
+            
+            
+            return 0;
+        }
+        static void Main(string[] args)
+        {
+
+            int firstNum = 10, secondNum = 5, add;
+            add = firstNum + secondNum;
+            Console.WriteLine("The sum is "+add);
+            Console.ReadKey();
+        }
+    }
+}
